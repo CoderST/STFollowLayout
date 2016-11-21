c3b7bba0ac95c42597c8df5f07929904856fe302
shengtao <694468528@qq.com>
2016-11-21 09:59:29 +0800

    初始化项目

---
+++ Demo/STFollowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STFollowLayout
+//
+//  Created by xiudou on 2016/11/21.
+//  Copyright © 2016年 xiudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
